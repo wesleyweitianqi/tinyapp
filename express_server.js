1424e2f5eccf92c7aeb55329fe8030f8a9f1f39d
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-11 17:23:01 -0400

    add a route and template

--- express_server.js
+++ express_server.js
@@ -28,6 +28,10 @@ app.get('/urls', (req,res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:id', (req,res) => {
+  const templateVars = {id: 'b2xVn2', longURL: "http://www.lighthouselabs.ca" };
+  res.render('urls_show', templateVars);
+})
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
