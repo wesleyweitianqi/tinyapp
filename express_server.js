ab625b8afef97d82ab07fd9052edcfb3efc4f70b
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-14 14:34:34 -0400

    more permission Feature

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@ const cookie = require('cookie-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8080; 
+app.set('view engine', 'ejs');
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const generateRandomString = function() {
   let string = '';
@@ -13,16 +17,27 @@ const generateRandomString = function() {
     string += lib[randomIndex];
   }
   return string;
-}
+};
 
-app.set('view engine', 'ejs');
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
+const urlsForUser = function(user_id) {
+  let newObj = {};
+  for (let i in urlDatabase) {
+    if (urlDatabase[i].userID === user_id) {
+      newObj[i] = urlDatabase[i];
+    }
+  }
+  return newObj;
+};
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 const users = {
@@ -37,7 +52,10 @@ const users = {
     password: "dishwasher-funk",
   },
 };
+
+//login emailLookUp
 const lookup = function(email) {
+  let arr = [];
   for (let i in users) {
     if (users[i]['email'] === email) {
       return users[i];
@@ -75,12 +93,15 @@ app.get('/urls.json', (req,res) => {
 // });
 
 app.get('/urls', (req,res) => {
-  if (!req.cookies.user_id) {
+  console.log(urlDatabase)
+  if (!req.cookies.user_id) {  // no cookie
     return res.redirect('/login')
   } else {
-    const templateVars = {urls: urlDatabase, email: users[req.cookies.user_id].email};
+    const validUser = urlsForUser(req.cookies.user_id)
+    console.log(req.cookies.user_id)
+    console.log(req.body)
+    const templateVars = {urls: validUser, email: users[req.cookies.user_id].email};
     res.render('urls_index', templateVars);
-
   }
 });
 
@@ -99,8 +120,8 @@ app.get('/urls/new', (req,res) => {
 //add shortUrl and LongUrl to index page
 app.post("/urls/new", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  let id = generateRandomString();
-  urlDatabase[id] = req.body.longURL;
+  let id = req.cookies.user_id;
+  urlDatabase[generateRandomString()] = {longURL: req.body.longURL, userID : id}
   res.redirect('/urls'); // Respond with 'Ok' (we will replace this)
 });
 
@@ -108,8 +129,10 @@ app.post("/urls/new", (req, res) => {
 app.get('/urls/:id', (req,res) => {
   if (!shortUrlLookup(req.params.id)) {
     return res.send("Page Not Found")
+  } else if (urlDatabase[req.params.id].userID !== req.cookies.user_id) {
+    return res.send("You donot have this shorten urls")
   } else {
-    const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+    const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id].longURL};
     console.log(users)
     res.render('urls_show', templateVars);
   }
@@ -117,7 +140,7 @@ app.get('/urls/:id', (req,res) => {
 
 app.post('/urls/:id', (req,res) => {
   const inputLongUrl = req.body.longURL;
-  urlDatabase[req.params.id] = req.body.longURL
+  urlDatabase[req.params.id] = {longURL: req.body.longURL, userID: req.cookies.user_id}
   res.redirect('/urls');
 })
 
@@ -135,7 +158,7 @@ app.post('/login', (req, res) => {
   if(lookup(req.body.email)) {
     if (lookup(req.body.email).password === req.body.password) {
       const value = lookup(req.body.email).id;
-      res.cookie('user_id', value)
+      res.cookie('user_id', lookup(req.body.email).id)
       return res.redirect('/urls')
     }
   }
@@ -159,10 +182,12 @@ app.post('/register', (req, res) => {
   }
   if (lookup(req.body.email)) {
     // if (lookup(req.body.email).password !== req.body.password) {
-      return res.sendStatus(400).end();
+      return res.send("This Email was reigistered");
     } else {
     const randomID = creatUserRandomID();
+    const shortenedID = generateRandomString()
     users[randomID] = {id: randomID, email: req.body.email, password: req.body.password}
+    //urlDatabase[shortenedID] = {email: req.body.email, userID:randomID}
     console.log(req.body)
     console.log(users)
     return res.cookie('user_id', randomID).redirect('/urls')
